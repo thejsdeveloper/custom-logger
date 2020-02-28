89035530dd31d8c6e8b611098c5504394167acab
vikas <thejsdeveloper@gmail.com>
2020-02-28 10:54:46 +0800

    imporoved logging

---
+++ logger/logger.js
@@ -0,0 +1,44 @@
+
+export default class Logger {
+
+    constructor(title) {
+
+        this.title = {
+            body: title || '---',
+            color: 'darkgrey',
+            size: '1rem'
+        };
+
+        this.body = {
+            color: '#008f68',
+            size: '1rem',
+            textShadow: `0 0 5px rgba(0, 0 ,0 , 0.2)`
+        };
+    }
+
+    setTiltleStyle({color, size}) {
+        if(!!color) this.title.color = color;
+        if(!!size) this.title.size = size;
+    }
+
+    setBodyStyle({color, size, textShadow}) {
+        if(!!color) this.body.color = color;
+        if(!!size) this.body.size = size;
+        if(!!textShadow) this.body.textShadow = textShadow;
+    }
+
+    log(body = '') {
+        console.log(
+            `%c${this.title.body} | ${body}`,
+            `color: ${this.title.color}; 
+            font-weight: bold; 
+            font-size: ${this.title.size};
+            `,
+            `color: ${this.body.color}; 
+            font-weight: bold; 
+            font-size: ${this.body.size};
+            text-shadow: ${this.body.textShadow}
+            `
+        );
+    }
+}
