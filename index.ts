633efe6762fa3f774d0502f5c2f637c3b2c690a6
thejsdeveloper <thejsdeveloper@gmail.com>
2020-02-28 10:57:15 +0800

    welcome message

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 // Import stylesheets
 import './style.css';
 
+import Logger from './logger/logger';
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+
+const log = new Logger().log;
+
+log(`Welcome to New Logger`)
+
